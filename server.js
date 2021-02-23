fe59dd74a8a0b19be0bdcdbf4f29c7c83c7d5aea
Nicki Baldwin <nickibaldwin@gmail.com>
2021-02-22 20:04:26 -0800

    added weather

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const PORT = process.env.PORT || 3009;
 //_______Routes______
 
 app.get('/location', handleGetLocation);
+app.get('/weather', handleGetWeather);
+
 function handleGetLocation(req, res) {
   console.log(req.query);
 
@@ -26,11 +28,27 @@ function handleGetLocation(req, res) {
   res.send(output);
 }
 
+function handleGetWeather(req, res) {
+
+  const data = require('./data/weather.json');
+  console.log(data.data[0].datetime);
+
+  const output = new Weather (data.data[0].weather.description, data.data[0].datetime);
+  console.log(output);
+
+  res.send(output);
+}
+
 function Location(dataFromTheFile, cityName) {
   this.search_query = cityName;
   this.fomatted_query = dataFromTheFile[0].display_name;
   this.latitude = dataFromTheFile[0].lat;
   this.longitude = dataFromTheFile[0].lon;
+}
+
+function Weather(description, time) {
+  this.forecast = description;
+  this.time = time;
 }
 
 //_______Initialization______
